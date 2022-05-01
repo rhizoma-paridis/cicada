53ca6489d116bc30aad4d31475177a8ec7ee0cdf
xupeiqi <xupeiqi@outlook.com>
2022-05-01 15:42:56 +0800

    init

---
+++ src/main/java/org/x/cicada/CicadaApplication.java
@@ -0,0 +1,13 @@
+package org.x.cicada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/x/cicada/CicadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.x.cicada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
